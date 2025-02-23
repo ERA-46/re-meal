1ac39272a7a9e8275d8ec7bd74874363baed8f12
erandajay <erandairushan98@gmail.com>
2025-02-22 22:32:54 -0800

    backend features - food request

--- remeal-backend/src/main/java/com/remeal/remeal_backend/controller/FoodRequestingController.java
+++ remeal-backend/src/main/java/com/remeal/remeal_backend/controller/FoodRequestingController.java
@@ -16,56 +16,58 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.remeal.remeal_backend.model.FoodListing;
+import com.remeal.remeal_backend.model.FoodRequest;
 import com.remeal.remeal_backend.model.User;
 import com.remeal.remeal_backend.repository.UserRepository;
-import com.remeal.remeal_backend.service.FoodListingService;
+import com.remeal.remeal_backend.service.FoodRequestService;
+
+
+
+
 
 @RestController
-@RequestMapping("api/food-listing")
-@CrossOrigin(origins = "http://localhost:3000")
-public class FoodListingController {
+@RequestMapping("/api/food-requesting")
+@CrossOrigin(origins="http://localhost:3000")
+public class FoodRequestingController {
 
     @Autowired
-    private FoodListingService foodListingService;
+    private FoodRequestService foodRequestService;
 
     @Autowired
     private UserRepository userRepository;
 
-
-
     @GetMapping("/")
-    public ResponseEntity<List<FoodListing>> getAllFoodListing(){
-        List<FoodListing> list = foodListingService.getAllFoodListing();
+    public ResponseEntity<List<FoodRequest>> getAllFoodRequests(){
+        List<FoodRequest> list = foodRequestService.getAllFoodRequests();
         return ResponseEntity.ok(list);
     }
 
-    @PostMapping("/list-food")
-    public ResponseEntity<FoodListing> createFoodListing(@RequestBody FoodListing foodListing){    
+    @PostMapping("/request-food")
+    public ResponseEntity<FoodRequest> createFoodRequest(@RequestBody FoodRequest request){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String name = authentication.getName();
 
         User currentUser =  userRepository.findByEmail(name)
-        .orElseThrow(() -> new RuntimeException("User not Logged in"));
-
-        Long supplierId = currentUser.getId();
+               .orElseThrow(() -> new RuntimeException("User not Logged in"));
         
-        FoodListing newFoodListing = foodListingService.createFoodListing(foodListing, supplierId);
-        return ResponseEntity.ok(newFoodListing);
+        Long requesterId = currentUser.getId();
+
+        FoodRequest newRequest = foodRequestService.createFoodRequest(request, requesterId);
+        return ResponseEntity.ok(newRequest);
     }
 
-    @PutMapping("/update-listing/{id}")
-    public ResponseEntity<FoodListing> updateFoodListing(@PathVariable Long id, @RequestBody FoodListing updatedFoodListing){
-        FoodListing foodListing = foodListingService.updateFoodListing(id, updatedFoodListing);        
-        return ResponseEntity.ok(foodListing);
+    @PutMapping("/update-request/{id}")
+    public ResponseEntity<FoodRequest> updateFoodRequest(@PathVariable Long id, @RequestBody FoodRequest updatedRequest){
+        FoodRequest request = foodRequestService.updateFoodRequest(id, updatedRequest);
+        return ResponseEntity.ok(request);
     }
 
-    @DeleteMapping("/delete-listing/{id}")
-    public ResponseEntity<Void> deleteFoodListing(@PathVariable Long id){
-        foodListingService.deleteFoodListing(id);
+    @DeleteMapping("/delete-request/{id}")
+    public ResponseEntity<Void> deleteFoodRequest(@PathVariable Long id){
+        foodRequestService.deleteFoodRequest(id);
         return ResponseEntity.noContent().build();
     }
-
-
     
+
+
 }
--- remeal-backend/src/main/java/com/remeal/remeal_backend/repository/FoodRequestRepository.java
+++ remeal-backend/src/main/java/com/remeal/remeal_backend/repository/FoodRequestRepository.java
@@ -1,7 +1,12 @@
 package com.remeal.remeal_backend.repository;
 
-import com.remeal.remeal_backend.model.FoodRequest;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.remeal.remeal_backend.model.FoodRequest;
+import com.remeal.remeal_backend.model.User;
+
 public interface FoodRequestRepository extends JpaRepository<FoodRequest, Long> {
+    List<FoodRequest> findByRequester(User requester);
 }

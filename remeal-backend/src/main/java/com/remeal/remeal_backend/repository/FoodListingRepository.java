a06cae60b385ee5e77a8f2f756f1d08d073172f5
asutsuki <fayb1157@gmail.com>
2025-04-11 16:34:38 -0700

    changed foodlistingrepo

--- remeal-backend/src/main/java/com/remeal/remeal_backend/repository/FoodListingRepository.java
+++ remeal-backend/src/main/java/com/remeal/remeal_backend/repository/FoodListingRepository.java
@@ -1,13 +1,19 @@
 package com.remeal.remeal_backend.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.remeal.remeal_backend.model.FoodListing;
 import com.remeal.remeal_backend.model.User;
 
-
 public interface FoodListingRepository extends JpaRepository<FoodListing, Long> {
+
     List<FoodListing> findBySupplier(User supplier);
+
+    @Query("SELECT new map(fl.supplier.name as supplier, SUM(fl.quantity) as totalQuantity) " +
+           "FROM FoodListing fl GROUP BY fl.supplier.name")
+    List<Map<String, Object>> getSupplierStats();
 }

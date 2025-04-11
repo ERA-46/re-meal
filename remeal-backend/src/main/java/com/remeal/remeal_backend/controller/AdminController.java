416345b8238a04b59f3cc718184b8ba9e186edef
asutsuki <fayb1157@gmail.com>
2025-04-11 16:34:32 -0700

    changed admincontroller

--- remeal-backend/src/main/java/com/remeal/remeal_backend/controller/AdminController.java
+++ remeal-backend/src/main/java/com/remeal/remeal_backend/controller/AdminController.java
@@ -1,13 +1,21 @@
 package com.remeal.remeal_backend.controller;
 
-import com.remeal.remeal_backend.model.User;
-import com.remeal.remeal_backend.model.FoodListing;
-import com.remeal.remeal_backend.service.UserService;
-import com.remeal.remeal_backend.service.FoodListingService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.remeal.remeal_backend.model.FoodListing;
+import com.remeal.remeal_backend.model.User;
+import com.remeal.remeal_backend.repository.FoodListingRepository;
+import com.remeal.remeal_backend.service.FoodListingService;
+import com.remeal.remeal_backend.service.UserService;   
 
 @RestController
 @RequestMapping("/api/admin")
@@ -36,10 +44,21 @@ public class AdminController {
     public List<FoodListing> getAllFoodListing() {
         return foodListingService.getAllFoodListing();
     }
+    
 
     // Delete a food listing by ID
     @DeleteMapping("/food-listings/{id}")
     public void deleteFoodListing(@PathVariable Long id) {
         foodListingService.deleteFoodListing(id);
     }
+    
+    
+    @Autowired
+    private FoodListingRepository foodListingRepository;
+    @GetMapping("/supplier/stats")
+    public ResponseEntity<List<Map<String, Object>>> getSupplierStats() {
+    List<Map<String, Object>> stats = foodListingRepository.getSupplierStats();
+    return ResponseEntity.ok(stats);
+    }
+
 }

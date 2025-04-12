9fa53b0e809b827f6dbee0fcd2bbc0f0b59fdab2
asutsuki <fayb1157@gmail.com>
2025-04-11 18:25:43 -0700

    remove update

--- remeal-backend/src/main/java/com/remeal/remeal_backend/config/SecurityConfig.java
+++ remeal-backend/src/main/java/com/remeal/remeal_backend/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -70,10 +69,5 @@ public class SecurityConfig {
         return source;
     }
 
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
     
 }
